9842cf28c01cce809ba0d999382ae9719123c7bf
sammedcjain <2020is_sammedcjain_b@nie.ac.in>
2023-11-22 12:16:39 +0530

    changed about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,19 @@
-
 function About() {
   return (
     <div>
       <h1 className="text-6xl mb-4">Github Finder</h1>
-      <p>Blah blah blahhh</p>
+      <h4 className="text-xl mb-2">
+        Github Finder is a web application built using :
+        <h4>React.js, Github's API and designed using Tailwind & Daisy UI</h4>
+        <br />
+        <p>Users can search for all Github users by typing their usernames, </p>
+        <p>
+          Users will also be able to see Github user's name, avatar, links, and
+          latest repos
+        </p>
+      </h4>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
